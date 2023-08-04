d1fb785c86a19e1509c7d089ed10893fe165880b
furkancak1r <102991095+furkancak1r@users.noreply.github.com>
2023-08-04 17:17:07 +0300

    3. Commit - 04.08.2023
    MySQL from https://www.freemysqlhosting.net/ is added and some functions about to modify the database with API requests are added.

---
+++ react_project_czm_1_mysql/config.js
@@ -0,0 +1,12 @@
+// Here is the part where the MySQL connection values are stored.
+
+// config.js dosyası
+const config = {
+    host: "sql7.freemysqlhosting.net",
+    user: "sql7637435",
+    password: "sIKCLcIep1",
+    database: "sql7637435",
+  };
+  
+  module.exports = config;
+  
--- react_project_czm_1_mysql/server.js
+++ react_project_czm_1_mysql/server.js
@@ -1,18 +1,56 @@
-const mysql = require('mysql');
+const express = require("express");
+const mysqlFunctions = require("./mysqlFunctions");
 
-// Veritabanına bağlan
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'navbardb'
+const app = express();
+const port = 3000;
+
+app.use(express.json()); // Middleware to parse JSON data
+
+app.post("/sqldata/createtable", (req, res) => {
+  // İstekten gelen verileri alıyoruz
+  const { table_names } = req.body;
+
+  // Create the table using the received values
+  mysqlFunctions.createTable(table_names[0]);
+
+  // Yanıt olarak 200 OK dönüyoruz
+  res.sendStatus(200);
 });
 
-// Veritabanına bağlanın
-connection.connect(function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Veritabanına bağlanıldı');
-  }
+app.post("/sqldata/droptable", (req, res) => {
+  // İstekten gelen verileri alıyoruz
+  const { table_names } = req.body;
+
+  // Drop the table using the received values
+  mysqlFunctions.dropTable(table_names[0]);
+
+  // Yanıt olarak 200 OK dönüyoruz
+  res.sendStatus(200);
 });
+
+app.post("/sqldata/insertrow", (req, res) => {
+  // İstekten gelen verileri alıyoruz
+  const { table_names, columns, values } = req.body;
+
+  // Insert the row using the received values
+  mysqlFunctions.insertRow(table_names, columns, values);
+
+  // Yanıt olarak 200 OK dönüyoruz
+  res.sendStatus(200);
+});
+
+app.post("/sqldata/selectrows", (req, res) => {
+  // İstekten gelen verileri alıyoruz
+  const { table_names, columns } = req.body;
+
+  // Select the rows using the received values
+  mysqlFunctions.selectRows(table_names, columns);
+
+  // Yanıt olarak 200 OK dönüyoruz
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
