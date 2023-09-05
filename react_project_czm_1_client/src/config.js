e9e3509f0bb182777c3e335fc3d326553270e962
furkancak1r <102991095+furkancak1r@users.noreply.github.com>
2023-09-05 11:58:49 +0300

    55. Commit - 05.09.2023
    dotenv is fixed in client side. Apparently react has REACT_APP_ thing.

--- react_project_czm_1_client/src/config.js
+++ react_project_czm_1_client/src/config.js
@@ -1,13 +1,7 @@
-// Here is the part where the MySQL connection values are stored.
-
-// config.js dosyası
-require('dotenv').config({path: './config.env'});
 
 const translateConfig = {
-  key: process.env.TRANSLATE_KEY,
-  endpoint: process.env.TRANSLATE_ENDPOINT,
-  location: process.env.TRANSLATE_LOCATION,
+  key: process.env.REACT_APP_TRANSLATE_KEY, 
+  endpoint: process.env.REACT_APP_TRANSLATE_ENDPOINT,
+  location: process.env.REACT_APP_TRANSLATE_LOCATION,
 };
-
-module.exports = translateConfig;
-
+export default translateConfig;
